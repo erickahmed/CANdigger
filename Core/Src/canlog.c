91218e782e459507cbfdd316d56ba9631d3766db
Erick Ahmed <engineering@erickahmed.com>
2026-06-17 18:05:45 +0200

    Include main header

--- Core/Src/canlog.c
+++ Core/Src/canlog.c
@@ -15,6 +15,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "canlog.h"
+#include "main.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "cmsis_os.h"
