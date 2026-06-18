691d5dc1eab94c07f7fab434d10541982d67d028
Erick Ahmed <engineering@erickahmed.com>
2026-06-18 08:49:52 +0200

    Fix baudrates
    - They were wrongly calculated on 168MHz but APB1 bus is at 42 MHz (168
      / 4)

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -277,7 +277,7 @@ static void MX_CAN1_Init(void)
 
   /* USER CODE END CAN1_Init 1 */
   hcan1.Instance = CAN1;
-  hcan1.Init.Prescaler = 4;
+  hcan1.Init.Prescaler = 8;
   hcan1.Init.Mode = CAN_MODE_SILENT;
   hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
   hcan1.Init.TimeSeg1 = CAN_BS1_16TQ;
@@ -314,7 +314,7 @@ static void MX_CAN2_Init(void)
 
   /* USER CODE END CAN2_Init 1 */
   hcan2.Instance = CAN2;
-  hcan2.Init.Prescaler = 8;
+  hcan2.Init.Prescaler = 16;
   hcan2.Init.Mode = CAN_MODE_SILENT;
   hcan2.Init.SyncJumpWidth = CAN_SJW_1TQ;
   hcan2.Init.TimeSeg1 = CAN_BS1_16TQ;
